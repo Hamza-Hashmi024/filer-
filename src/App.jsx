496b79593be1a64b9db37b97ad579002451091fc
Hamza-Hashmi024 <hamza064@gmail.com>
2024-12-05 16:23:41 +0500

    update Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import { BrowserRouter , Routes , Route } from "react-router-dom";
-import ScrollToTop from './ScrollToTop';
-import Home from "./Pages/Home";
-import Aboutus from "./Pages/Aboutus";
-import Contactus from "./Pages/Contactus";
-import BusinessServices from "./Pages/BusinessServices";
-import TaxTools from "./Components/TaxTools";
-import NtNStatus from "./Tools/NtNStatus";
-import ATLstatus from "./Tools/ATLstatus";
 
-
+import  UserRoutes from "./Components/userRoutes"
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <ScrollToTop/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/aboutus" element={<Aboutus/>}/>
-      <Route path="/contactus" element={<Contactus/>}/>
-      <Route path="/services" element={<BusinessServices/>}/>
-      <Route path="/taxtools" element={<TaxTools/>}/>
-      <Route path="/ntnstatuschecker" element={<NtNStatus/>}/>
-      <Route path="/Atlstatus" element={<ATLstatus/>}/>
-    </Routes>
-    </BrowserRouter>
+
+    <>
+    <UserRoutes/>
+    </>
+   
   )
 }
 
---
+++ src/Components/userRoutes.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import ScrollToTop from '../ScrollToTop';
+import Home from "../Pages/Home";
+import Aboutus from "../Pages/Aboutus";
+import Contactus from "../Pages/Contactus";
+import BusinessServices from "../Pages/BusinessServices";
+import TaxTools from "../Components/TaxTools";
+import NtNStatus from "../Tools/NtNStatus"
+import ATLstatus from "../Tools/ATLstatus";
+
+const userRoutes = () => {
+  return (
+    <BrowserRouter>
+    <ScrollToTop/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/aboutus" element={<Aboutus/>}/>
+      <Route path="/contactus" element={<Contactus/>}/>
+      <Route path="/services" element={<BusinessServices/>}/>
+      <Route path="/taxtools" element={<TaxTools/>}/>
+      <Route path="/ntnstatuschecker" element={<NtNStatus/>}/>
+      <Route path="/Atlstatus" element={<ATLstatus/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default userRoutes
